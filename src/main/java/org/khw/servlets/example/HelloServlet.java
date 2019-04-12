88104dbe60df9fe64b3414b7c1952df04438e12e
khw <cokecan@daum.net>
2019-04-12 14:45:35 +0900

    추가

---
+++ src/main/java/org/khw/servlets/example/HelloServlet.java
@@ -0,0 +1,22 @@
+package org.khw.servlets.example;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet{
+	
+	public void doGet(HttpServletRequest request,HttpServletResponse response)
+	 throws ServletException, IOException{
+		String name = "김형욱";
+				
+				request.setAttribute("name", name);
+		request.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(request,response);
+	}
+
+	
+
+}
