461b49963968d05d014fd9a390051f8b3ee713cd
khw <cokecan@daum.net>
2019-04-19 11:29:56 +0900

    추가

--- src/main/java/org/khw/servlets/example/HelloServlet.java
+++ src/main/java/org/khw/servlets/example/HelloServlet.java
@@ -3,10 +3,11 @@ package org.khw.servlets.example;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+@WebServlet("/hello")
 public class HelloServlet extends HttpServlet{
 	
 	public void doGet(HttpServletRequest request,HttpServletResponse response)
